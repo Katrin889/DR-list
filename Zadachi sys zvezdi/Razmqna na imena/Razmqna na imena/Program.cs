4e7ab8d4bc274d1b0c8883b5b29bf10685a23db1
Katrin889 <kakamitreva@gmail.com>
2025-03-20 08:07:46 +0200

    Add files via upload

---
+++ Zadachi sys zvezdi/Razmqna na imena/Razmqna na imena/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Razmqna_na_imena
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Въведи имена:");
+            List<string> list = Console.ReadLine().Split(',').ToList();
+            list.Reverse();
+            for (int i = 0; i < list.Count; i++)
+            {
+                string[] words = list[i].Split(' ');
+                list[i] = words[1] + " " + words[0];
+            }
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+        }
+    }
+}
